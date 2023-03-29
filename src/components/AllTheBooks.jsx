f49123d97c720b95506cf7b3cf9cad1f264d7ec8
Luigi160397 <luigi.difraia.97@gmail.com>
2023-03-29 16:47:39 +0200

    last-commit

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -2,17 +2,77 @@ import { Component } from "react";
 
 import { Container, Row, Col, Card } from "react-bootstrap";
 
-import books from "../books/fantasy.json";
+import books1 from "../books/fantasy.json";
+import books2 from "../books/history.json";
+import books3 from "../books/horror.json";
+import books4 from "../books/romance.json";
+import books5 from "../books/scifi.json";
 
 class AllTheBooks extends Component {
   render() {
     return (
-      <Container>
-        <h2 className="display-4 pt-5">Sfoglia i nostri libri:</h2>
-        <Row xs="2" md="2" lg="4" className="pt-3 pb-5">
-          {books.map((book, index) => (
+      <Container className="pb-5">
+        <h2 className="display-4 pt-5">Sezione fantasy:</h2>
+        <Row style={{ overflow: "auto" }} className="pt-3 flex-nowrap">
+          {books1.map((book, index) => (
             <Col key={`Book-${index}`} className="pb-3 px-2">
-              <Card className="shadow">
+              <Card className="shadow libri">
+                <Card.Img variant="top" className="img-fluid" id="image-card" src={book.img} />
+                <Card.Body>
+                  <Card.Title className="text-truncate">{book.title}</Card.Title>
+                  <Card.Text>Prezzo: {book.price} €</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <h2 className="display-4 pt-5">Sezione history:</h2>
+        <Row style={{ overflow: "auto" }} className="pt-3 flex-nowrap">
+          {books2.map((book, index) => (
+            <Col key={`Book-${index}`} className="pb-3 px-2">
+              <Card className="shadow libri">
+                <Card.Img variant="top" className="img-fluid" id="image-card" src={book.img} />
+                <Card.Body>
+                  <Card.Title className="text-truncate">{book.title}</Card.Title>
+                  <Card.Text>Prezzo: {book.price} €</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <h2 className="display-4 pt-5">Sezione horror:</h2>
+        <Row style={{ overflow: "auto" }} className="pt-3 flex-nowrap">
+          {books3.map((book, index) => (
+            <Col key={`Book-${index}`} className="pb-3 px-2">
+              <Card className="shadow libri">
+                <Card.Img variant="top" className="img-fluid" id="image-card" src={book.img} />
+                <Card.Body>
+                  <Card.Title className="text-truncate">{book.title}</Card.Title>
+                  <Card.Text>Prezzo: {book.price} €</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <h2 className="display-4 pt-5">Sezione Romance:</h2>
+        <Row style={{ overflow: "auto" }} className="pt-3 flex-nowrap">
+          {books4.map((book, index) => (
+            <Col key={`Book-${index}`} className="pb-3 px-2">
+              <Card className="shadow libri">
+                <Card.Img variant="top" className="img-fluid" id="image-card" src={book.img} />
+                <Card.Body>
+                  <Card.Title className="text-truncate">{book.title}</Card.Title>
+                  <Card.Text>Prezzo: {book.price} €</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <h2 className="display-4 pt-5">Sezione Shi-fi:</h2>
+        <Row style={{ overflow: "auto" }} className="pt-3 flex-nowrap">
+          {books5.map((book, index) => (
+            <Col key={`Book-${index}`} className="pb-3 px-2">
+              <Card className="shadow libri">
                 <Card.Img variant="top" className="img-fluid" id="image-card" src={book.img} />
                 <Card.Body>
                   <Card.Title className="text-truncate">{book.title}</Card.Title>
