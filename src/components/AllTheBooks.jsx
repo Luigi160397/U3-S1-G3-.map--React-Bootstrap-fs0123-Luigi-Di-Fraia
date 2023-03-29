02d4da55d01a01073cba4f34a796719f8c174158
Luigi160397 <luigi.difraia.97@gmail.com>
2023-03-29 16:12:29 +0200

    index

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -8,9 +8,10 @@ class AllTheBooks extends Component {
   render() {
     return (
       <Container>
-        <Row xs="2" md="2" lg="4" className="py-5">
-          {books.map(book => (
-            <Col key={`Book-${book.id}`} className="pb-3 px-2">
+        <h2 className="display-4 pt-5">Sfoglia i nostri libri:</h2>
+        <Row xs="2" md="2" lg="4" className="pt-3 pb-5">
+          {books.map((book, index) => (
+            <Col key={`Book-${index}`} className="pb-3 px-2">
               <Card className="shadow">
                 <Card.Img variant="top" className="img-fluid" id="image-card" src={book.img} />
                 <Card.Body>
